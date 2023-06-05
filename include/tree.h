c9a0cf00439ecb14b8cc8fa2489c15c5df794a5d
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-06-05 15:01:31 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -4,38 +4,38 @@
 #include <vector>
 #include <algorithm>
 class Tree {
-  private:
-  std::vector<std::vector<char>> list;
-  struct Node {
-    char value = '\0';
-    std::vector<Node*> childs;
-  };
-  Node* root = nullptr;
-  std::vector<char> data;
-  Node* create(char value) {
-    Node* temp = new Node;
-    temp->value = value;
-    return temp;
-  }
-  Node* createTree(Node* root, std::vector<char> data) {
-    if (root == nullptr) {
-      root = new Node;
+   private:
+    std::vector<std::vector<char>> list;
+    struct Node {
+      char value = '\0';
+      std::vector<Node*> childs;
+    };
+    Node* root = nullptr;
+    std::vector<char> data;
+    Node* create(char value) {
+      Node* temp = new Node;
+      temp->value = value;
+      return temp;
     }
-    for (int i = 0; i < data.size(); i++) {
-      Node* ptr = create(data[i]);
-      root->childs.push_back(ptr);
-      std::vector<char> upd(data);
-      upd.erase(std::find(upd.begin(), upd.end(), data[i]));
-      createTree(ptr, upd);
+    Node* createTree(Node* root, std::vector<char> data) {
+      if (root == nullptr) {
+        root = new Node;
+      }
+      for (int i = 0; i < data.size(); i++) {
+        Node* ptr = create(data[i]);
+        root->childs.push_back(ptr);
+        std::vector<char> upd(data);
+        upd.erase(std::find(upd.begin(), upd.end(), data[i]));
+        createTree(ptr, upd);
+      }
+      return root;
     }
-    return root;
-  }
-  public:
-  std::vector<std::vector<char>> getPermut() const {
-    return list;
-  }
-  explicit Tree(std::vector<char>);
-  void Perm(Node*, std::vector<char>);
+   public:
+    std::vector<std::vector<char>> getPermut() const {
+      return list;
+    }
+    explicit Tree(std::vector<char>);
+    void Perm(Node*, std::vector<char>);
 };
 Tree::Tree(std::vector<char> vec) {
   root = createTree(root, vec);
